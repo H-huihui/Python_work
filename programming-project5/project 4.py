03039fee8db11cfd3e676a4bf70dfb26bb227a89
Masoon <han706964362@gmail.com>
2015-04-01 23:06:34 +0800

    Create project 4.py

---
+++ programming-project5/project 4.py
@@ -0,0 +1,36 @@
+from project3 import *
+                            
+	#YOUR CODE GOES HERE
+def draw(width,height,letter,number):
+	result=""
+	temp=create(width,height,number)
+	test=0
+	for i in range(1,width*height+1):
+		if(temp[test]==i):
+			if(i%width==0):
+				result=result+letter+"\n"
+			else:
+				result=result+letter
+			test=test+1
+		else:
+			if(i%width==0):
+				result=result+" "+"\n"
+			else:
+				result=result+" "			
+	return result		
+	
+def soften(grid):
+        count=0
+        i=0
+        while(i<len(grid)):
+                j=0
+                while(j<len(grid[i])):
+                      if(grid[i][j]!="_"):
+                              count=count+1         
+                      j=j+1
+                i=i+1
+        if(grid[1][1]=='_'):
+                count=count
+        else:
+                count=count-1
+        return count
